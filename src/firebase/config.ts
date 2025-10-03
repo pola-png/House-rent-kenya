ff4f80860c18424a2dd96ad32860c3c63556458a
Edie Gathegi <ediegathegi5@gmail.com>
2025-10-03 16:41:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4644281358-6a533",
+  "appId": "1:829620954687:web:c5a8d320fd352ddb4f7a8a",
+  "apiKey": "AIzaSyA4seR93xoti4nHTd8giVd9aL1MGtB2j78",
+  "authDomain": "studio-4644281358-6a533.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "829620954687"
+};
